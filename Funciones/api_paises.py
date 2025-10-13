8cfd93ce74c0fcb413171bbcfe0ca3d4a1448bea
Matias Soto <matiassoto00@outlook.com>
2025-10-13 21:24:23 +0000

    api

--- Funciones/api_paises.py
+++ Funciones/api_paises.py
@@ -1,1 +1,14 @@
-print ("probando")
+import requests
+
+
+
+try:
+    res = requests.get("https://www.apicountries.com/countries")
+    res.raise_for_status()
+    todo = res.json()
+    nombres = list(map(lambda e: e["name"], todo))
+    print ("names", nombres)
+
+
+except requests.exceptions.RequestException as error:
+    print("Error", error)  
