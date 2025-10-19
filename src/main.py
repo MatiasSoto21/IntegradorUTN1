c4c47be34cd4935320d4a7c42a9d51499ab32fde
Matias Soto <matiassoto00@outlook.com>
2025-10-19 06:52:41 +0000

    mejorando consola estetica

--- src/main.py
+++ src/main.py
@@ -1,18 +1,22 @@
 from funciones.paises import get_paises
 from funciones.filtrados import filtrar_continente
 from funciones.buscar import buscar_pais
+from rich.console import Console # type: ignore
 
 bandera: bool = True
+console = Console()
 
 while bandera:
-    respuesta = input("""
-          Bienvenido a la app de Paises!
+    respuesta = console.input("""
+          [bold][underline]Bienvenido a la app de Paises![/bold][/underline]
+                              
           Elija la opcion deseada
           ---------------------------------------------------
-          1)Buscar Pais
-          2)Filtrar paises
+          1) Buscar Pais
+          2) Filtrar paises
           3)
-          4)SALIR
+          4) SALIR
+                          
 """)
     get_paises()
     
@@ -25,4 +29,4 @@ while bandera:
     elif respuesta == "4":
         bandera = False
         print("ADIOS")
-    else: print("ERROR, opcion no valida, intente de nuevo")
+    else: console.print("[red]ERROR, opcion no valida, intente de nuevo")
